c32f89bd5d76fd2729903ef728fd3a1e975bff60
白茶清欢 <go_developer@163.com>
2025-08-09 12:32:30 +0800

    save code

--- context.go
+++ context.go
@@ -117,6 +117,7 @@ func (c *Context) reset() {
 // This has to be used when the context has to be passed to a goroutine.
 func (c *Context) Copy() *Context {
 	cp := Context{
+		mu:        sync.RWMutex{},
 		writermem: c.writermem,
 		Request:   c.Request,
 		engine:    c.engine,
